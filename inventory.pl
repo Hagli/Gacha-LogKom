06f3a4534c0047eb6f385c4cc5378845c6b1f6af
Christian A.T <54507598+chrslex@users.noreply.github.com>
2020-11-23 15:55:57 +0700

    Update inventory.pl

--- inventory.pl
+++ inventory.pl
@@ -1,19 +1,20 @@
 /* inventory.pl */
 /* Mendefinisikan inventory dengan 100 space */
-:- dynamic(space_filled/1).
-inventory(X) :- space_filled(X).
+:- dynamic(space_Filled/1).
+inventory(X) :- space_Filled(X).
+space_Filled(0).
 
 /* Cek item di inventory */
 :- dynamic(kept/2).
 check_inventory(X) :- kept(X,Y), Y > 0.
 
 /*Definisi menyimpan item */
-item_save(X):-  (inventory(filled), filled < 100 ->
+itemSave(X):-  (inventory(Filled), Filled < 100 ->
                 (kept(X,Y) -> retract(kept(X,Y)), Z is Y+1,
-                asserta(kept(X,Z)), retract(filledSpace(filled)),
-                Newfilled is filled + 1, asserta(filledSpace(Newfilled));
-                asserta(kept(X,1)), retract(filledSpace(filled)),
-                Newfilled is filled + 1, asserta(filledSpace(Newfilled))),
+                asserta(kept(X,Z)), retract(space_Filled(Filled)),
+                NewFilled is Filled + 1, asserta(space_Filled(NewFilled));
+                asserta(kept(X,1)), retract(space_Filled(Filled)),
+                NewFilled is Filled + 1, asserta(space_Filled(NewFilled))),
                 write('Item telah disimpan ke dalam inventory.'), nl;
                 write('Inventory penuh.'), nl).
 /* Menghitung jumlah item spesifik */
