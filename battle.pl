075af659d3395562f76d8c77df9e60078d2a5fb2
Hagli <hagli2001@gmail.com>
2020-11-25 21:55:10 +0700

    nambahin command special attack
    special attack basically multiple normal attack (klo mau digantik sok aja, entah kenapa kungodingnya gabisa)

--- battle.pl
+++ battle.pl
@@ -1,4 +1,5 @@
 :- dynamic(enemy/5).
+:- dynamic(cooldown/1).
 get_enemy :-
 	random(0,99,X),
 	player(_,_,_,_,_,Level,_,_,_,_,_),
@@ -26,18 +27,49 @@ attack :-
 	player(_,_,_,_,_,_,_,Y,_,_,_),
 	call(enemy(A,Lv,HP,Att,Def)),
 	random(0,5,Plus),
-	Z is Y + 3 - Def + Plus,
+	Z is Y + 6 - Def + Plus,
+	(Z<0 -> Z is 0;Z is Z),
 	X is HP - Z,
-	write('Deal '),write(Z),write(' damage'),nl,nl,
+	(Z =< 0 -> (write('Musuh '),write(A),write('menghindari seranganmu!'));
+	(write('Deal '),write(Z),write(' damage'))),nl,
 	assertz(enemy(A,Lv,X,Att,Def)),
 	retract(enemy(A,Lv,HP,Att,Def)).
+enemy_attack :-
+	enemy(A,Lv,Hp,Att,Def),
+	call(player(O,N,M,L,K,J,I,H,G,F,E)),
+	random(0,3,Plus),
+	Z is Att + 3 - G + Plus,
+	(Z<0 -> Z is 0; Z is Z),
+	X is F - Z,
+	(Z =< 0 -> (write('Kau menghindari serangan musuh '), write(A));
+	(write('Musuh '), write(A),write(' deal '),write(Z),write(' damage'))),
+	nl,nl,
+	assertz(player(O,N,M,L,K,J,I,H,G,X,E)),
+	retract(player(O,N,M,L,K,J,I,H,G,F,E)).
+sp_attack_do(1) :-
+	attack.
+sp_attack_do(Z) :-
+	attack,
+	Y is Z-1,
+	sp_attack_do(Y).
+sp_attack :-
+	cooldown(0),
+	random(2,5,Z),
+	sp_attack_do(Z),
+	assertz(cooldown(3)),
+	retract(cooldown(0)).
+sp_attack :-
+	cooldown(X),
+	write('Special attack tidak bisa digunakan!'),nl,
+	write('Cooldown '),write(X),write(' turn').
 /*attack :-
 	call(enemy(A,Lv,HP,Att,Def)),
 	retract(enemy(A,Lv,HP,Att,Def)),!.*/
 battle_loop :-
 	call(enemy(A,Y,Hp,Att,Def)),
-	Hp =< 0,write('Musuh telah dikalahkan'),
-	retract(enemy(A,Y,Hp,Att,Def)),!.
+	Hp =< 0,write('Musuh '),write(A),write(' telah dikalahkan'),
+	retract(enemy(A,Y,Hp,Att,Def)),
+	retract(cooldown(X)),!.
 battle_loop :-
 	call(enemy(A,Y,Hp,Att,Def)),
 	call(player(_,_,_,_,_,_,_,B,C,D,E)),
@@ -52,10 +84,13 @@ battle_loop :-
 	read(Choice),nl,
 	(
         Choice='attack' -> attack;
-		Choice=2 -> randompick
-    ),battle_loop.
+		Choice='sp_attack' -> sp_attack;
+		battle_loop
+    ),call(enemy(_,_,H,_,_)),
+	(H>0 -> enemy_attack; battle_loop),battle_loop.
 start_battle :-
 	get_enemy,
+	asserta(cooldown(0)), /*untuk sp_attack cooldown*/
 	call(enemy(A,Y,Hp,Att,Def)),
 	write('Sebuah level '),write(Y),write(' '),write(A), write(' telah muncul!'),nl,
 	battle_loop.
